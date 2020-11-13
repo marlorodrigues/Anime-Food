bc8ce66150cce71e1c75b67e359ebc695f18cc0f
marlorodrigues <49593773+marlorodrigues@users.noreply.github.com>
2020-11-13 20:55:31 -0300

    hg

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import './style.css';
-import { Link } from 'react-router-dom';
-import lamen from './lamen-naruto.jpeg'
-import porkBacon from './pork-bacon.jpeg'
-import ramen from './ramen-shokugeki.jpeg'
-import rolinhoPrimavera from './rolinho-primavera.jpeg'
-import comida from './sei-la.jpeg'
-import bentou from './bentou.jpeg'
 // import omelete from './omelete.jpeg'
 
 export default class Main extends Component {
     state = {
         repositorios: [],
-        carrinho: [],
-        page: 1
     };
 
     componentDidMount() { //Executa uma acao assim q o componente e renderizado 
@@ -22,29 +13,11 @@ export default class Main extends Component {
     }
 
     loadProducts = async (page = 1) => { //Para funcoes criadas usar a Arrow Function p/ poder usar o .this
-        const response = await api.get('/marlorodrigues/repos');
+        console.log("Indo fazer a requisicao");
+        const response = await api.get('showAll');
         // const response = await api.get('/marlorodrigues/repos?page={page}'); //Se a requisicao trouxesse mais de uma pagina
 
-        this.setState({ repositorios: response.data, page }); //Atualiza o estado atual 
-    }
-
-    prevPage = () => { //Volta uma pagina
-        const { page } = this.state;
-
-        if (page === 1) return;
-
-        // const pageNumber = page - 1;
-        // this.loadProducts(pageNumber);
-    }
-
-    nextPage = () => { //Avanca uma pagina
-        const { page } = this.state;
-
-        if (page === 1) return;
-
-        // const pageNumber = page + 1;
-
-        // this.loadProducts(pageNumber);
+        this.setState({ repositorios: response.data["AllThings"][0], page }); //Atualiza o estado atual 
     }
 
     addOnLittleCar = (e, idProduct) => {
@@ -53,69 +26,21 @@ export default class Main extends Component {
     }
 
     render() { //Renderiza o componente
-        const { page } = this.state;
+        const { repositorios } = this.state;
 
         return (
             <div className="product-list">
                 <div className="grid-container">
                     <div className="item1">
-                        <img src={lamen} id="img-item1" alt="imagem de comida" />
-                        <p id="description-food">Este e so um teste para testar coisas que precisam ser testadas. Pois se nao forem testadas talvez nao funcionem.</p>
+                        <p id="description-food">Last: {repositorios.name}</p>
                         <div id="price-container">
-                            <p id="name-price">Preço</p>
-                            <p id="price">R$ 24,90</p>
+
                         </div>
-                        <Link to="/" onClick={(e) => this.addOnLittleCar(e, 200)}>Quero esse!</Link>
-                    </div>
-                    <div className="item1">
-                        <img src={porkBacon} id="img-item1" alt="imagem de comida" />
-                        <p id="description-food">Este e so um teste para testar coisas que precisam ser testadas. Pois se nao forem testadas talvez nao funcionem.</p>
-                        <div id="price-container">
-                            <p id="name-price">Preço</p>
-                            <p id="price">R$ 18,90</p>
-                        </div>
-                        <Link to="/" onClick={(e) => this.addOnLittleCar(e, 300)}>Quero esse!</Link>
-                    </div>
-                    <div className="item1">
-                        <img src={ramen} id="img-item1" alt="imagem de comida" />
-                        <p id="description-food">Este e so um teste para testar coisas que precisam ser testadas. Pois se nao forem testadas talvez nao funcionem.</p>
-                        <div id="price-container">
-                            <p id="name-price">Preço</p>
-                            <p id="price">R$ 28,90</p>
-                        </div>
-                        <Link href="#">Quero esse!</Link>
-                    </div>
-                    <div className="item1">
-                        <img src={rolinhoPrimavera} id="img-item1" alt="imagem de comida" />
-                        <p id="description-food">Este e so um teste para testar coisas que precisam ser testadas. Pois se nao forem testadas talvez nao funcionem.</p>
-                        <div id="price-container">
-                            <p id="name-price">Preço</p>
-                            <p id="price">R$ 19,90</p>
-                        </div>
-                        <Link href="#">Quero esse!</Link>
-                    </div>
-                    <div className="item1">
-                        <img src={comida} id="img-item1" alt="imagem de comida" />
-                        <p id="description-food">Este e so um teste para testar coisas que precisam ser testadas. Pois se nao forem testadas talvez nao funcionem.</p>
-                        <div id="price-container">
-                            <p id="name-price">Preço</p>
-                            <p id="price">R$ 34,90</p>
-                        </div>
-                        <Link href="#">Quero esse!</Link>
-                    </div>
-                    <div className="item1">
-                        <img src={bentou} id="img-item1" alt="imagem de comida" />
-                        <p id="description-food">Este e so um teste para testar coisas que precisam ser testadas. Pois se nao forem testadas talvez nao funcionem.</p>
-                        <div id="price-container">
-                            <p id="name-price">Preço</p>
-                            <p id="price">R$ 12,90</p>
-                        </div>
-                        <Link href="#">Quero esse!</Link>
                     </div>
                 </div>
                 <div className="actions">
-                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
-                    <button disabled={page === 1} onClick={this.nextPage}>Proximo</button> {/* No meu caso so ha uma pagina, se houvesse outra, e so puxar pela api */}
+                    {/* <button disabled={page === 1} onClick={this.prevPage}>Anterior</button> */}
+                    {/* <button disabled={page === 1} onClick={this.nextPage}>Proximo</button> No meu caso so ha uma pagina, se houvesse outra, e so puxar pela api */}
                 </div>
             </div >
 
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // const api = axios.create({ baseURL: 'localhost:8080/api' });
-const api = axios.create({ baseURL: 'https://api.github.com/users/' });
+const api = axios.create({ baseURL: 'http://localhost:3000/' });
 
 export default api;
 
