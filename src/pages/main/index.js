d6a0da50649fa7099a02b42d2970378878064ed0
marlorodrigues <49593773+marlorodrigues@users.noreply.github.com>
2020-11-14 23:27:04 -0300

    Pegando img do DB

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -9,15 +9,18 @@ export default class Main extends Component {
     };
 
     componentDidMount() { //Executa uma acao assim q o componente e renderizado 
-        this.loadProducts();
+        // this.loadProducts();
     }
 
     loadProducts = async (page = 1) => { //Para funcoes criadas usar a Arrow Function p/ poder usar o .this
         console.log("Indo fazer a requisicao");
-        const response = await api.get('showAll');
+        const response = await api.get('image');
         // const response = await api.get('/marlorodrigues/repos?page={page}'); //Se a requisicao trouxesse mais de uma pagina
+        console.log("Feita -> " + response.data.status._image);
 
-        this.setState({ repositorios: response.data["AllThings"][0], page }); //Atualiza o estado atual 
+        document.getElementById('imgElem').setAttribute('src', response.data.status._image);
+        // this.setState({ repositorios: response.data["AllThings"][0], page }); //Atualiza o estado atual 
+        this.setState({ repositorios: response.data.status._image }); //Atualiza o estado atual 
     }
 
     addOnLittleCar = (e, idProduct) => {
@@ -26,19 +29,20 @@ export default class Main extends Component {
     }
 
     render() { //Renderiza o componente
-        const { repositorios } = this.state;
+        // const { repositorios } = this.state;
+
 
         return (
             <div className="product-list">
                 <div className="grid-container">
                     <div className="item1">
-                        <p id="description-food">Last: {repositorios.name}</p>
-                        <div id="price-container">
-
-                        </div>
+                        <p id="description-food">Last: Teste</p>
+                        <img id="imgElem" alt=""></img>
                     </div>
+
                 </div>
                 <div className="actions">
+                    <button onClick={this.loadProducts}>Anterior</button>
                     {/* <button disabled={page === 1} onClick={this.prevPage}>Anterior</button> */}
                     {/* <button disabled={page === 1} onClick={this.nextPage}>Proximo</button> No meu caso so ha uma pagina, se houvesse outra, e so puxar pela api */}
                 </div>
